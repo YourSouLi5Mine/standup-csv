a6123e09388c53b4c6efc5a6a3672805f8b054ba
Jorge Precich <precichysim@gmail.com>
2019-02-12 11:33:00 -0600

    delete unused methods

--- array.rb
+++ array.rb
@@ -1,28 +1,7 @@
 class Array
-  def pretty
-    self.each do |row|
-      print row
-      puts
-    end
-  end
+  puts 'Array class loaded!'
 
-  def to_struct
-    arr_struct = [self[0]]
-    city = Struct.new(*self[0].map {|value| value.to_sym})
-    self[1..-1].each { |row| arr_struct << city.new(*row) }
-    arr_struct
-  end
-
-  def sort_arr
-    self[1..-1] = self[1..-1].sort! { |a, b| a[8] <=> b[8] }
-    self
-  end
-
-  def change_col(index, value)
-    self[1..-1].each do |row| 
-      row[index] = value
-      value = value.succ
-    end
-    self
+  def sort_arr(col)
+    self.sort! { |a, b| a.send(col) <=> b.send(col) }
   end
 end
